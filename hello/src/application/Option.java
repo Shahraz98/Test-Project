91348487f193c136427f968ca251363dde101ba1
shahraz.nasir@stud-inf.unibz.it <shahraz.nasir@stud-inf.unibz.it>
2020-04-24 13:49:25 +0200

    testproj

---
+++ hello/src/application/Option.java
@@ -0,0 +1,18 @@
+package application;
+
+public enum Option {
+	Director, Employee;
+	
+	private Option() {
+		
+	}
+	
+	public String value() {
+		return name();
+	}
+	public static Option fromvalue(String val) {
+		
+		return valueOf(val);
+	}
+
+}
---
+++ hello/src/dbUtil/dbConnection.java
@@ -0,0 +1,28 @@
+package dbUtil;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {
+	
+	private static final String SQLCONN = "jdbc:sqlite:Bsystem.sqlite";
+
+	public static Connection getConnection()
+			throws SQLException{
+		
+		try
+	    {
+	      Class.forName("org.sqlite.JDBC");
+	      
+	      return DriverManager.getConnection(SQLCONN);
+	    }
+	    catch (ClassNotFoundException|SQLException ex)
+	    {
+	      ex.printStackTrace();
+	    }
+	    return null;
+	}
+}
+
